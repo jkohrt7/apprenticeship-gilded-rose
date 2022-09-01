231fe4db9eb2850d805ac8de70d720c4a913040b
Jared Kohrt <jared.kohrt@heysparkbox.com>
2022-09-01 14:32:19 -0400

    test: settled on a more verbose test format

--- src/inventory/gilded_rose.spec.js
+++ src/inventory/gilded_rose.spec.js
@@ -1,89 +1,89 @@
 import { Item, updateQuality } from './gilded_rose';
 
 describe('`updateQuality`', () => {
-  let standardItem;
-  let legendaryItem;
-  let cheeseItem;
-  let ticketItem;
-
   //'Haunted Shoe' is a generic item and represents default behavior.
-  it('deprecates sell_in and quality of standard items by one each update', () => {
-    standardItem = new Item('Haunted Shoe', 10, 10);
+  it('deprecates the sell in by one for a Haunted Shoe', () => {
+    const standardItem = new Item('Haunted Shoe', 10, 10);
     updateQuality([standardItem]);
     expect(standardItem.sell_in).toBe(9);
+  });
+
+  it('deprecates the quality by one for a Haunted Shoe while sell in is >=0', () => {
+    const standardItem = new Item('Haunted Shoe', 10, 10);
+    updateQuality([standardItem]);
     expect(standardItem.quality).toBe(9);
   });
 
-  //Legendary items' sell_in values should not change and they should not decrease in quality.
-  it('does not lower the quality or sell_in of a Hand of Ragnaros', () => {
-    legendaryItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+  it('deprecates the quality by 2 for a Haunted Shoe while sell in is <0', () => {
+    const standardItem = new Item('Haunted Shoe', 0, 10);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(8);
+  });
+
+  it('does not lower quality of a Haunted Shoe below 0', () => {
+    const standardItem = new Item('Haunted Shoe', 10, 0);
+    updateQuality([standardItem]);
+    expect(standardItem.quality).toBe(0);
+  });
+
+  //Legendary items do not have sell by dates or decrease in quality.
+  //I am unsure if they are also supposed to have quality >50, as shown here.
+  it('does not lower the quality of a Hand of Ragnaros', () => {
+    const legendaryItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
     updateQuality([legendaryItem]);
     expect(legendaryItem.quality).toBe(80);
+  })
+
+  it('does not lower the sell_in of a Hand of Ragnaros', () => {
+    const legendaryItem = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+    updateQuality([legendaryItem]);
     expect(legendaryItem.sell_in).toBe(0);
   })
 
-  //Aged Brie is meant to increase in quality as sell_in decreases.
-  it('increments the quality of aged brie by 1 as sell_in decreases', () => {
-    cheeseItem = new Item('Aged Brie', 10, 10);
+  //Aged Brie is meant to increase in quality as sell by decreases. It 
+  //should cap at 50 quality.
+  it('increments the quality of aged brie by 1 as sell-in decreases', () => {
+    const cheeseItem = new Item('Aged Brie', 10, 10);
     updateQuality([cheeseItem]);
     expect(cheeseItem.quality).toBe(11);
+  })
+
+  it('increments the quality of aged brie by 2 when sell-in <0', () => {
+    const cheeseItem = new Item('Aged Brie', 0, 10);
+    updateQuality([cheeseItem]);
+    expect(cheeseItem.quality).toBe(12);
+  })
+
+  it('cannot increment the quality of aged brie once it is 50', () => {
+    const cheeseItem = new Item('Aged Brie', 50, 50);
+    updateQuality([cheeseItem]);
+    expect(cheeseItem.quality).toBe(50);
   })
 
   //Tickets go up by 2 quality once sell-in <=10, but their quality is locked 
   //at 0 once sell-in <=0. They are otherwise identical to aged brie.
   it('increments the quality of tickets by 1 while sell-in >10', () => {
-    ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10);
+    const ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10);
     updateQuality([ticketItem]);
     expect(ticketItem.quality).toBe(11);
   })
 
   it('increments the quality of tickets by 2 while sell-in <=10', () => {
-    ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+    const ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10);
     updateQuality([ticketItem]);
     expect(ticketItem.quality).toBe(12);
   })
 
   it('sets the quality of tickets to 0 when sell-in is 0', () => {
-    ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10);
+    const ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10);
     updateQuality([ticketItem]);
     expect(ticketItem.quality).toBe(0);
   })
 
-  //Quality is never a negative value
-  it('does not decrement quality below 0 for any item type', () => {
-    standardItem = new Item('Haunted Shoe', 0, 0);
-    cheeseItem = new Item('Aged Brie', 0, 0);
-    ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0);
-    legendaryItem = new Item('Sulfuras, Hand of Ragnaros', 0, 0);
-
-    updateQuality([standardItem,cheeseItem,ticketItem,legendaryItem])
-
-    expect(standardItem.quality).toBe(0);
-    expect(cheeseItem.quality).toBe(2);
+  it('does not change quality after sell-in drops below 0', () => {
+    const ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10);
+    updateQuality([ticketItem]);
+    updateQuality([ticketItem]);
     expect(ticketItem.quality).toBe(0);
-    expect(legendaryItem.quality).toBe(0);
-  })
-
-  //Quality caps at 50; Initial value can be >50, however.
-  it('does not increment quality above 50 for any appreciating items', () => {
-    cheeseItem = new Item('Aged Brie', 50, 50);
-    ticketItem = new Item('Backstage passes to a TAFKAL80ETC concert', 50, 50);
-
-    updateQuality([cheeseItem,ticketItem])
-
-    expect(cheeseItem.quality).toBe(50);
-    expect(ticketItem.quality).toBe(50);
-  })
-
-  //Changes in quality double past sell_in date; note that
-  //this behavior is overridden for legendary items and tickets
-  it('doubles changes in quality when sell_in is <0', () => {
-    standardItem = new Item('Haunted Shoe', 0, 10);
-    cheeseItem = new Item('Aged Brie', 0, 0);
-
-    updateQuality([standardItem, cheeseItem]);
-
-    expect(standardItem.quality).toBe(8);
-    expect(cheeseItem.quality).toBe(2);
   })
 });
